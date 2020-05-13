acd44f2f9f6fce3b71200db0f240ef5f082e71aa
xxm-cf <chunfu.guo@hekr.me>
2020-05-13 17:55:14 +0800

    Update:  更新绑点

--- src/views/config/index.vue
+++ src/views/config/index.vue
@@ -52,7 +52,7 @@
       ref="workspace"
       :globalCanvasConfig="globalCanvasConfig"
       :imgList="imgList"
-      :deviceList="demoDeviceList")
+      :deviceList="deviceList")
 </template>
 
 <script >
